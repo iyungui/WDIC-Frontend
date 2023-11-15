b6a8fbff7478c359a837551cc4da990336a9f313
iyungui <yungui632@gmail.com>
2023-11-15 13:15:51 +0900

    Initial Commit

---
+++ WDIC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WDIC
+//
+//  Created by 이융의 on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WDIC/WDICApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDICApp.swift
+//  WDIC
+//
+//  Created by 이융의 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct WDICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
